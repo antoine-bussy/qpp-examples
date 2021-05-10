08147b9b6e0d2014bb28ed6c1ad848a5d1c3427f
Antoine Bussy <bussyantoine@gmail.com>
2021-05-10 15:20:27 +0200

    chapter1 - add z-gate example

--- qpp-examples/nielsen_chuang/chapter1/chapter1.3.test.cpp
+++ qpp-examples/nielsen_chuang/chapter1/chapter1.3.test.cpp
@@ -23,6 +23,25 @@ TEST(chapter1_3, not_gate)
     {
         std::cerr << ">> State:\n" << qpp::disp(state) << '\n';
         std::cerr << ">> NOT Gate:\n" << qpp::disp(qpp::gt.X) << '\n';
-        std::cerr << ">> NOT State:\n" << qpp::disp(state) << '\n';
+        std::cerr << ">> NOT State:\n" << qpp::disp(not_state) << '\n';
+    }
+}
+
+//! @brief Equation 1.13
+TEST(chapter1_3, z_gate)
+{
+    using namespace qpp::literals;
+    auto const state = qpp::randket(2).eval();
+    auto const z_state = (qpp::gt.Z * state).eval();
+
+    EXPECT_EQ(z_state[0], state[0]);
+    EXPECT_EQ(z_state[1],-state[1]);
+    EXPECT_MATRIX_EQ(qpp::gt.Z, Eigen::Vector2cd(1, -1).asDiagonal().toDenseMatrix());
+
+    if constexpr (print_text)
+    {
+        std::cerr << ">> State:\n" << qpp::disp(state) << '\n';
+        std::cerr << ">> Z Gate:\n" << qpp::disp(qpp::gt.Z) << '\n';
+        std::cerr << ">> Z State:\n" << qpp::disp(z_state) << '\n';
     }
 }
