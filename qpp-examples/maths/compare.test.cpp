be1f1090f39495cc33ffc1602580b521f1ba2a90
Antoine Bussy <bussyantoine@gmail.com>
2021-05-13 17:42:09 +0200

    maths - fix compare tests

--- qpp-examples/maths/compare.test.cpp
+++ qpp-examples/maths/compare.test.cpp
@@ -21,9 +21,9 @@ TEST(compare_test, matrix_close)
 TEST(compare_test, complex_close)
 {
     using namespace std::complex_literals;
-    EXPECT_TRUE(qpp_e::maths::complex_close(1. + 1i, 1. + 1.01i, 0.01));
-    EXPECT_TRUE(qpp_e::maths::complex_close_l(1. + 1i, 1. + 1.01i, 0.01));
-    EXPECT_FALSE(qpp_e::maths::complex_close(1. + 1i, 1. + 1.1i, 0.01));
-    EXPECT_FALSE(qpp_e::maths::complex_close_l(1. + 1i, 1. + 1.1i, 0.01));
+    EXPECT_TRUE(qpp_e::maths::complex_close(1. + 1i, 1. + 1.01i, 0.001));
+    EXPECT_TRUE(qpp_e::maths::complex_close_l(1. + 1i, 1. + 1.01i, 0.001));
+    EXPECT_FALSE(qpp_e::maths::complex_close(1. + 1i, 1. + 1.1i, 0.001));
+    EXPECT_FALSE(qpp_e::maths::complex_close_l(1. + 1i, 1. + 1.1i, 0.001));
 }
 
