9756292867a09ada4cdbc18d3dc509f6c801cb2d
Antoine Bussy <bussyantoine@gmail.com>
2021-05-29 23:27:14 +0200

    maths - fix power function

--- qpp-examples/maths/arithmetic.hpp
+++ qpp-examples/maths/arithmetic.hpp
@@ -12,13 +12,16 @@ namespace qpp_e::maths
 
     inline auto constexpr pow(arithmetic auto const& base, std::integral auto const& exp) -> std::decay_t<decltype(base)>
     {
+        using base_t = std::decay_t<decltype(base)>;
         assert(exp >= 0);
+        if constexpr (std::is_signed_v<base_t>)
+            if (base == -1)
+                return (exp % 2 == 0) ? 1 : -1;
+
         if (base == 2)
             return (1 << exp);
-        else if (base == -1)
-            return (exp % 2 == 0) ? 1 : -1;
-        else
-            return std::pow(base, exp);
+
+        return std::pow(base, exp);
     }
 
 } /* namespace qpp_e::maths */
