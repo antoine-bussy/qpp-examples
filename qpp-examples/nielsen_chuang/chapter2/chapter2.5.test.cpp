3638f2afb91e85b87f9a524b274313670be1d5a9
Antoine Bussy <bussyantoine@gmail.com>
2021-08-22 19:51:38 +0200

    chapter 2 - add example of schmidt decomposition proof when dimensions are different

--- qpp-examples/nielsen_chuang/chapter2/chapter2.5.test.cpp
+++ qpp-examples/nielsen_chuang/chapter2/chapter2.5.test.cpp
@@ -154,3 +154,76 @@ TEST(chapter2_5, schmidt_decomposition_proof)
     EXPECT_GT(D.minCoeff(), -1e-12);
     EXPECT_COMPLEX_CLOSE(D.squaredNorm(), 1., 1e-12);
 }
+
+//! @brief Exercise 2.76
+TEST(chapter2_5, schmidt_decomposition_proof_different_dimensions)
+{
+    qpp_e::maths::seed(1982u);
+
+    auto constexpr n = 4u;
+    auto constexpr _2_pow_n = qpp_e::maths::pow(2u, n);
+    auto constexpr range_n = std::views::iota(0u, _2_pow_n) | std::views::common;
+    auto constexpr m = 3u;
+    auto constexpr _2_pow_m = qpp_e::maths::pow(2u, m);
+    auto constexpr range_m = std::views::iota(0u, _2_pow_m) | std::views::common;
+    auto constexpr _2_pow_npm = _2_pow_n * _2_pow_m;
+    auto constexpr policy = std::execution::par;
+
+    auto const a = qpp::randket(_2_pow_n);
+    auto const b = qpp::randket(_2_pow_m);
+    auto const A = (a * b.transpose()).eval();
+    auto const psi = qpp::kron(a, b);
+    EXPECT_COMPLEX_CLOSE(psi.squaredNorm(), 1., 1e-12);
+    EXPECT_EQ(psi.size(), _2_pow_npm);
+
+    auto const psi2 = std::transform_reduce(policy, range_n.begin(), range_n.end()
+        , Eigen::VectorXcd::Zero(_2_pow_npm).eval()
+        , std::plus<>{}
+        , [&](auto&& j)
+    {
+        return std::transform_reduce(std::execution::seq, range_m.begin(), range_m.end()
+            , Eigen::VectorXcd::Zero(_2_pow_npm).eval()
+            , std::plus<>{}
+            , [&](auto&& k)
+        {
+            return (A(j, k) * qpp::kron(Eigen::VectorXcd::Unit(_2_pow_n, j), Eigen::VectorXcd::Unit(_2_pow_m, k))).eval();
+        });
+    });
+    EXPECT_MATRIX_CLOSE(psi2, psi, 1e-12);
+    EXPECT_COMPLEX_CLOSE(psi2.squaredNorm(), 1., 1e-12);
+
+    auto const svd = A.bdcSvd(Eigen::ComputeFullU | Eigen::ComputeFullV);
+    auto const& U = svd.matrixU();
+    auto const& D = svd.singularValues();
+    auto const  V = svd.matrixV().adjoint();
+
+    EXPECT_MATRIX_CLOSE(A, U.leftCols(_2_pow_m) * D.asDiagonal() * V, 1e-12);
+    EXPECT_MATRIX_CLOSE(U * U.adjoint(), Eigen::MatrixXcd::Identity(_2_pow_n, _2_pow_n), 1e-12);
+    EXPECT_MATRIX_CLOSE(V * V.adjoint(), Eigen::MatrixXcd::Identity(_2_pow_m, _2_pow_m), 1e-12);
+
+    for (auto&& j : range_n)
+        for (auto&& k : range_m)
+        {
+            auto const A_jk = std::transform_reduce(std::execution::seq, range_m.begin(),range_m.end()
+                , std::complex{ 0. }
+                , std::plus<>{}
+                , [&](auto&& i)
+            {
+                return U(j, i) * D[i] * V(i, k);
+            });
+            EXPECT_COMPLEX_CLOSE(A_jk, A(j, k), 1e-12);
+        }
+
+    auto const psi3 = std::transform_reduce(policy, range_m.begin(), range_m.end()
+        , Eigen::VectorXcd::Zero(_2_pow_npm).eval()
+        , std::plus<>{}
+        , [&](auto&& i)
+    {
+        return (D[i] * qpp::kron(U.col(i), V.row(i).transpose())).eval();
+    });
+    EXPECT_MATRIX_CLOSE(psi3, psi, 1e-12);
+    EXPECT_COMPLEX_CLOSE(psi3.squaredNorm(), 1., 1e-12);
+
+    EXPECT_GT(D.minCoeff(), -1e-12);
+    EXPECT_COMPLEX_CLOSE(D.squaredNorm(), 1., 1e-12);
+}
