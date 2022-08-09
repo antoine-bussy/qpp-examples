54c094ddc60c583138216457dddbb84420c83292
Antoine Bussy <bussyantoine@gmail.com>
2022-08-09 07:46:55 +0200

    chapter 2 - add chapter 2.6 file

--- qpp-examples/nielsen_chuang/chapter2/SourcesLists.cmake
+++ qpp-examples/nielsen_chuang/chapter2/SourcesLists.cmake
@@ -3,4 +3,5 @@ set(chapter2_test_sources
     chapter2.3.test.cpp
     chapter2.4.test.cpp
     chapter2.5.test.cpp
+    chapter2.6.test.cpp
 )
---
+++ qpp-examples/nielsen_chuang/chapter2/chapter2.6.test.cpp
@@ -0,0 +1,14 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include <qpp/qpp.h>
+
+namespace
+{
+    auto constexpr print_text = false;
+}
+
+//! @brief Box 2.7
+TEST(chapter2_6, anti_correlations)
+{
+}
