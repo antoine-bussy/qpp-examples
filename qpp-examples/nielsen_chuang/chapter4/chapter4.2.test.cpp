7614ed7c3c814a2d4158cf943e2f51bd4ebad6b7
Antoine Bussy <bussyantoine@gmail.com>
2022-08-18 11:47:51 +0200

    chapter 4 - add important single qubit gates

--- qpp-examples/nielsen_chuang/chapter4/chapter4.2.test.cpp
+++ qpp-examples/nielsen_chuang/chapter4/chapter4.2.test.cpp
@@ -15,17 +15,49 @@ namespace
     auto constexpr print_text = false;
 }
 
-//! @brief Equation
-TEST(chapter4_2, dummy)
+//! @brief Equations 4.1 through 4.3
+TEST(chapter4_2, important_single_qubit_gates)
 {
-    auto constexpr n = 3u;
-    auto constexpr _2_pow_n = qpp_e::maths::pow(2u, n);
+    using namespace std::literals::complex_literals;
+    using namespace std::numbers;
 
-    auto const state = qpp::randket(_2_pow_n).eval();
-    EXPECT_MATRIX_CLOSE(state, state, 1e-12);
+    auto constexpr inv_sqrt2 = 0.5 * sqrt2;
+
+    auto const I = Eigen::Matrix2cd::Identity();
+    EXPECT_MATRIX_CLOSE(qpp::gt.Id2, I, 1e-12);
+
+    auto const X = Eigen::Matrix2cd{{0., 1.}, {1., 0.}};
+    EXPECT_MATRIX_CLOSE(qpp::gt.X, X, 1e-12);
+
+    auto const Y = Eigen::Matrix2cd{{0., -1i}, {1i, 0.}};
+    EXPECT_MATRIX_CLOSE(qpp::gt.Y, Y, 1e-12);
+
+    auto const Z = Eigen::Matrix2cd{{1., 0.}, {0., -1.}};
+    EXPECT_MATRIX_CLOSE(qpp::gt.Z, Z, 1e-12);
+
+    auto const H = (inv_sqrt2 * Eigen::Matrix2cd{{1., 1.}, {1., -1.}}).eval();
+    EXPECT_MATRIX_CLOSE(qpp::gt.H, H, 1e-12);
+
+    auto const S = Eigen::Matrix2cd{{1., 0.}, {0., 1i}};
+    EXPECT_MATRIX_CLOSE(qpp::gt.S, S, 1e-12);
+
+    auto const T = Eigen::Matrix2cd{{1., 0.}, {0., std::exp(0.25i * pi)}};
+    EXPECT_MATRIX_CLOSE(qpp::gt.T, T, 1e-12);
+
+    auto const TT = (std::exp(0.125i * pi) * Eigen::Matrix2cd{{std::exp(-0.125i * pi), 0.}, {0., std::exp(0.125i * pi)}}).eval();
+    EXPECT_MATRIX_CLOSE(TT, T, 1e-12);
+
+    EXPECT_MATRIX_CLOSE((inv_sqrt2 * (X + Z)).eval(), H, 1e-12);
+    EXPECT_MATRIX_CLOSE((T*T).eval(), S, 1e-12);
 
     if constexpr (print_text)
     {
-        std::cerr << ">> State:\n" << qpp::disp(state) << '\n';
+        std::cerr << ">> I:\n" << qpp::disp(I) << "\n\n";
+        std::cerr << ">> X:\n" << qpp::disp(X) << "\n\n";
+        std::cerr << ">> Y:\n" << qpp::disp(Y) << "\n\n";
+        std::cerr << ">> Z:\n" << qpp::disp(Z) << "\n\n";
+        std::cerr << ">> H:\n" << qpp::disp(H) << "\n\n";
+        std::cerr << ">> S:\n" << qpp::disp(S) << "\n\n";
+        std::cerr << ">> T:\n" << qpp::disp(T) << "\n\n";
     }
 }
