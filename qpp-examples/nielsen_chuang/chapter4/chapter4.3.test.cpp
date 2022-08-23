c42f123f19d11a25ae18360df452b363feae18f1
Antoine Bussy <bussyantoine@gmail.com>
2022-08-23 19:18:54 +0200

    chapter 4 - add controlled-U example

--- qpp-examples/nielsen_chuang/chapter4/chapter4.3.test.cpp
+++ qpp-examples/nielsen_chuang/chapter4/chapter4.3.test.cpp
@@ -63,3 +63,51 @@ TEST(chapter4_3, cnot_circuit)
         std::cerr << ">> expected_out: " << qpp::disp(expected_out.transpose()) << "\n";
     }
 }
+
+//! @brief Figure 4.4
+TEST(chapter4_3, controlled_u)
+{
+    using namespace qpp::literals;
+
+    qpp_e::maths::seed();
+
+    auto const U = qpp::randU();
+
+    auto cU = Eigen::Matrix4cd::Identity().eval();
+    cU({ 2, 3 }, { 2, 3 }) = U;
+    auto const controlled_U = qpp::gt.CTRL(U, { 0 }, { 1 }, 2);
+    EXPECT_MATRIX_EQ(cU, controlled_U);
+
+    auto circuit = qpp::QCircuit{ 2, 0 };
+    circuit.gate_joint(controlled_U, { 0, 1 });
+    auto engine = qpp::QEngine{ circuit };
+
+    /* Random |c>|t> input */
+    auto const c = qpp::randket();
+    auto const t = qpp::randket();
+
+    auto const in = qpp::kron(c,t);
+    engine.reset().set_psi(in).execute();
+    auto const out = engine.get_psi();
+
+    auto const expected_out = (cU * in).eval();
+    EXPECT_MATRIX_CLOSE(out, expected_out, 1e-12);
+
+    /* |0>|t> input */
+    engine.reset().set_psi(qpp::kron(0_ket,t)).execute();
+    EXPECT_MATRIX_CLOSE(engine.get_psi(), qpp::kron(0_ket,t), 1e-12);
+
+    /* |1>|t> input */
+    engine.reset().set_psi(qpp::kron(1_ket,t)).execute();
+    EXPECT_MATRIX_CLOSE(engine.get_psi(), qpp::kron(1_ket, (U*t).eval()), 1e-12);
+
+    if constexpr (print_text)
+    {
+        std::cerr << ">> Circuit:\n" << circuit << "\n\n" << circuit.get_resources() << "\n\n";
+        std::cerr << ">> Engine:\n" << engine << "\n\n";
+        std::cerr << ">> c: " << qpp::disp(c.transpose()) << "\n";
+        std::cerr << ">> t: " << qpp::disp(t.transpose()) << "\n";
+        std::cerr << ">> out: " << qpp::disp(out.transpose()) << "\n";
+        std::cerr << ">> expected_out: " << qpp::disp(expected_out.transpose()) << "\n";
+    }
+}
