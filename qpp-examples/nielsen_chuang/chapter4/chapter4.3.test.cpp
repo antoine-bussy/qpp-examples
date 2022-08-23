10b7168412bb942f6bb19e5834bd8ab82bf067fe
Antoine Bussy <bussyantoine@gmail.com>
2022-08-23 19:10:01 +0200

    chapter 4 - add example of CNOT circuit

--- qpp-examples/nielsen_chuang/chapter4/chapter4.3.test.cpp
+++ qpp-examples/nielsen_chuang/chapter4/chapter4.3.test.cpp
@@ -15,11 +15,51 @@ namespace
     auto constexpr print_text = false;
 }
 
-//! @brief Equations ??
-TEST(chapter4_3, dummy)
+//! @brief Equation 4.23
+TEST(chapter4_3, cnot_circuit)
 {
+    using namespace qpp::literals;
+    qpp_e::maths::seed();
+
+    auto const cnot = Eigen::Matrix4cd
+    {
+        { 1., 0., 0., 0. },
+        { 0., 1., 0., 0. },
+        { 0., 0., 0., 1. },
+        { 0., 0., 1., 0. },
+    };
+    EXPECT_MATRIX_EQ(cnot, qpp::gt.CNOT);
+
+    auto circuit = qpp::QCircuit{ 2, 0 };
+    circuit.gate_joint(qpp::gt.CNOT, { 0, 1 });
+    auto engine = qpp::QEngine{ circuit };
+
+    /* Random |c>|t> input */
+    auto const c = qpp::randket();
+    auto const t = qpp::randket();
+
+    auto const in = qpp::kron(c,t);
+    engine.reset().set_psi(in).execute();
+    auto const out = engine.get_psi();
+
+    auto const expected_out = (cnot * in).eval();
+    EXPECT_MATRIX_CLOSE(out, expected_out, 1e-12);
+
+    /* |0>|t> input */
+    engine.reset().set_psi(qpp::kron(0_ket,t)).execute();
+    EXPECT_MATRIX_CLOSE(engine.get_psi(), qpp::kron(0_ket,t), 1e-12);
+
+    /* |1>|t> input */
+    engine.reset().set_psi(qpp::kron(1_ket,t)).execute();
+    EXPECT_MATRIX_CLOSE(engine.get_psi(), qpp::kron(1_ket,t.reverse()), 1e-12);
+
     if constexpr (print_text)
     {
-        std::cerr << ">> dummy:\n" << true << "\n\n";
+        std::cerr << ">> Circuit:\n" << circuit << "\n\n" << circuit.get_resources() << "\n\n";
+        std::cerr << ">> Engine:\n" << engine << "\n\n";
+        std::cerr << ">> c: " << qpp::disp(c.transpose()) << "\n";
+        std::cerr << ">> t: " << qpp::disp(t.transpose()) << "\n";
+        std::cerr << ">> out: " << qpp::disp(out.transpose()) << "\n";
+        std::cerr << ">> expected_out: " << qpp::disp(expected_out.transpose()) << "\n";
     }
 }
