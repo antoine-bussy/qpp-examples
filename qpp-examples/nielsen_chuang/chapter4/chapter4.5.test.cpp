1ffac7a93871cfbd3cbc7050b41125bc57e2c1bf
Antoine Bussy <bussyantoine@gmail.com>
2022-09-28 16:20:55 +0200

    chapter 4 - add example of two-level unitary decomposition

--- qpp-examples/nielsen_chuang/chapter4/chapter4.5.test.cpp
+++ qpp-examples/nielsen_chuang/chapter4/chapter4.5.test.cpp
@@ -58,3 +58,93 @@ TEST(chapter4_5, addition_mod_4)
     }
 }
 
+namespace
+{
+    struct two_level_matrix_t
+    {
+        Eigen::Index index = 0;
+        Eigen::Matrix2cd U = Eigen::Matrix2cd::Identity();
+    };
+
+    auto two_level_unitary_decomposition(qpp_e::maths::Matrix auto const& U, std::vector<two_level_matrix_t>& out, Eigen::Index const offset = 0) -> void
+    {
+        using namespace Eigen::indexing;
+
+        debug() << ">> U:\n" << qpp::disp(U) << "\n\n";
+
+        auto const n = U.cols();
+        assert(n >= 2);
+
+        if(n == 2)
+        {
+            out.emplace_back(offset, U);
+            return;
+        }
+
+        auto const range = std::views::iota(0, n-1) | std::views::common;
+
+        auto const I2 = Eigen::Matrix2cd::Identity();
+        auto const I = Eigen::MatrixXcd::Identity(n, n);
+        EXPECT_MATRIX_CLOSE((U.adjoint()*U).eval(), I, 1.e-12);
+        EXPECT_MATRIX_CLOSE((U*U.adjoint()).eval(), I, 1.e-12);
+
+        auto new_U = U.eval();
+        for(auto&& i : range | std::views::reverse)
+        {
+            auto const& b = new_U(i+1, 0);
+            if (b == 0.)
+                continue;
+            auto const& a = new_U(i, 0);
+
+            auto s = two_level_matrix_t{};
+            s.index = i + offset;
+            s.U.col(0) = Eigen::Vector2cd{ a, b };
+            s.U.col(1) = Eigen::Vector2cd{ -std::conj(b), std::conj(a) };
+            s.U /= std::sqrt(std::norm(a) + std::norm(b));
+            EXPECT_MATRIX_CLOSE((s.U.adjoint()*s.U).eval(), I2, 1.e-12);
+            EXPECT_MATRIX_CLOSE((s.U*s.U.adjoint()).eval(), I2, 1.e-12);
+
+            out.emplace_back(s);
+            new_U({i, i+1}, all) = s.U.adjoint() * new_U({i, i+1}, all);
+        }
+        debug() << ">> new_U:\n" << qpp::disp(new_U) << "\n\n";
+
+        return two_level_unitary_decomposition(new_U(lastN(n-1), lastN(n-1)), out, offset + 1);
+    }
+
+    auto two_level_unitary_decomposition(qpp_e::maths::Matrix auto const& U)
+    {
+        auto result = std::vector<two_level_matrix_t>{};
+        two_level_unitary_decomposition(U, result);
+        return result;
+    }
+}
+
+//! @brief Equations 4.41 through 4.51
+TEST(chapter4_5, two_level_unitary_decomposition)
+{
+    qpp_e::maths::seed();
+
+    auto constexpr n = 8;
+    auto const U = qpp::randU(n);
+
+    auto const u = two_level_unitary_decomposition(U);
+
+    auto constexpr multiply = [](qpp_e::maths::Matrix auto const& M, two_level_matrix_t const& s)
+    {
+        auto P = Eigen::MatrixXcd::Identity(n, n).eval();
+        auto const& i = s.index;
+        P({i, i+1}, {i, i+1}) = s.U;
+
+        debug() << ">> P:\n" << qpp::disp(P) << "\n\n";
+
+        return (M * P).eval();
+    };
+
+    auto const computed_U = std::accumulate(u.cbegin(), u.cend(), Eigen::MatrixXcd::Identity(n, n).eval(), multiply);
+
+    EXPECT_MATRIX_CLOSE(computed_U, U, 1.e-12);
+
+    debug() << ">> computed_U:\n" << qpp::disp(computed_U) << "\n\n";
+    debug() << ">> U:\n" << qpp::disp(U) << "\n\n";
+}
