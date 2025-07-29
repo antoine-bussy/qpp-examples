c238d7c92611cab761cbea71ebda634e5c1df456
Antoine Bussy <bussyantoine@gmail.com>
2025-07-29 09:42:10 +0200

    chapter 4 - use extract_matrix

--- qpp-examples/nielsen_chuang/chapter4/chapter4.5.2.test.cpp
+++ qpp-examples/nielsen_chuang/chapter4/chapter4.5.2.test.cpp
@@ -5,6 +5,7 @@
 
 #include <qpp/qpp.hpp>
 #include <qpp-examples/qube/debug.hpp>
+#include <qpp-examples/qube/introspection.hpp>
 #include <qpp-examples/maths/arithmetic.hpp>
 #include <qpp-examples/maths/random.hpp>
 #include <qpp-examples/maths/gtest_macros.hpp>
@@ -33,15 +34,7 @@ TEST(chapter4_5, single_qubit_and_cnot_universality)
         .CTRL(X, {0,2}, 1, std::vector{1ul,0ul})
         .CTRL(X, {0,1}, 2, std::vector{1ul,1ul})
         ;
-    auto engine = qpp::QEngine{ circuit };
-
-    auto U = Eigen::MatrixXcd::Zero(n, n).eval();
-    for(auto&& i : std::views::iota(0ul, n))
-    {
-        auto const state = Eigen::Vector<std::complex<double>,8>::Unit(i).eval();
-        engine.set_state(state).execute();
-        U.col(i) = engine.get_state();
-    }
+    auto const U = qube::extract_matrix<8>(circuit);
 
     EXPECT_MATRIX_CLOSE(U, U_expected, 1e-12);
     debug() << ">> U:\n" << qpp::disp(U) << "\n\n";
@@ -68,15 +61,7 @@ TEST(chapter4_5, single_qubit_and_cnot_universality_2)
         .CTRL(U_tilde, {1,2}, 0)
         .CTRL(X, {0,1}, 2, std::vector{1ul,0ul})
         ;
-    auto engine = qpp::QEngine{ circuit };
-
-    auto U = Eigen::MatrixXcd::Zero(n, n).eval();
-    for(auto&& i : std::views::iota(0ul, n))
-    {
-        auto const state = Eigen::Vector<std::complex<double>,8>::Unit(i).eval();
-        engine.set_state(state).execute();
-        U.col(i) = engine.get_state();
-    }
+    auto const U = qube::extract_matrix<8>(circuit);
 
     EXPECT_MATRIX_CLOSE(U, U_expected, 1e-12);
     debug() << ">> U:\n" << qpp::disp(U) << "\n\n";
