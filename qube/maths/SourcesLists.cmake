2c79917dd96a286c7f98bc46517be710a9494abd
Antoine Bussy <bussyantoine@gmail.com>
2025-07-29 10:32:11 +0200

    maths - use target_sources

--- qube/maths/SourcesLists.cmake
+++ qube/maths/SourcesLists.cmake
@@ -1,17 +1,26 @@
-set(maths_headers
-    arithmetic.hpp
-    compare.hpp
-    concepts.hpp
-    random.hpp
+# maths library
+target_sources(maths
+    INTERFACE
+        FILE_SET interface_headers TYPE HEADERS
+            arithmetic.hpp
+            compare.hpp
+            concepts.hpp
+            random.hpp
 )
 
-set(maths_test_sources
-    arithmetic.test.cpp
-    compare.test.cpp
-    concepts.test.cpp
-    random.test.cpp
+# maths unit test
+target_sources(maths.test
+    PRIVATE
+        FILE_SET sources
+            arithmetic.test.cpp
+            compare.test.cpp
+            concepts.test.cpp
+            random.test.cpp
 )
 
-set(maths_test_tools_headers
-    gtest_macros.hpp
+# maths test tools
+target_sources(maths.test_tools
+    INTERFACE
+        FILE_SET interface_headers TYPE HEADERS
+            gtest_macros.hpp
 )
