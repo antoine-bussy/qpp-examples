34323b1741dff07e22e127eafac15cdf646b0b19
Antoine Bussy <bussyantoine@gmail.com>
2025-07-30 15:33:01 +0200

    chapter 4 - add example of probability error boundness

--- qpp-examples/nielsen_chuang/chapter4/chapter4.5.3.test.cpp
+++ qpp-examples/nielsen_chuang/chapter4/chapter4.5.3.test.cpp
@@ -36,3 +36,40 @@ TEST(chapter4_5, operator_error)
         EXPECT_LT(e, error + epsilon);
     }
 }
+
+//! @brief Equation 4.62, and equations 4.64 through 4.68
+TEST(chapter4_5_2, probability_error_bound)
+{
+    qube::maths::seed();
+
+    auto constexpr nq = 3ul;
+    auto constexpr n = qube::maths::pow(2ul, nq);
+    auto constexpr m = 5ul;
+
+    auto const U = qpp::randU(n);
+    auto const V = qpp::randU(n);
+    auto const error = qube::maths::operator_norm_2(U - V);
+
+    auto const Ks = qpp::randkraus(m, n);
+    auto const M = (Ks[2].adjoint() * Ks[2]).eval();
+
+    auto constexpr epsilon = 1e-12;
+
+    for([[maybe_unused]] auto&& i: std::views::iota(0, 50))
+    {
+        auto const psi = qpp::randket(n);
+        auto const [P_U, P_U_imag] = (U * psi).dot(M * (U * psi));
+        auto const [P_V, P_V_imag] = (V * psi).dot(M * (V * psi));
+
+        // Check that P_U and P_V are probabilities
+        EXPECT_GT(P_U, -epsilon);
+        EXPECT_LT(P_U, 1. + epsilon);
+        EXPECT_LT(std::abs(P_U_imag), epsilon);
+        EXPECT_GT(P_V, -epsilon);
+        EXPECT_LT(P_V, 1. + epsilon);
+        EXPECT_LT(std::abs(P_V_imag), epsilon);
+
+        EXPECT_LT(std::abs(P_U - P_V), 2 * error + epsilon);
+    }
+
+}
