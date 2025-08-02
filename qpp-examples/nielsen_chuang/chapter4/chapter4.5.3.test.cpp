3b8e1cf7e922aff020b0c0937e3a71d5090ba1bd
Antoine Bussy <bussyantoine@gmail.com>
2025-08-02 14:57:05 +0200

    chapter 4 - add end for quantum gates universality

--- qpp-examples/nielsen_chuang/chapter4/chapter4.5.3.test.cpp
+++ qpp-examples/nielsen_chuang/chapter4/chapter4.5.3.test.cpp
@@ -9,6 +9,8 @@
 #include <qube/maths/gtest_macros.hpp>
 #include <qube/maths/norm.hpp>
 #include <qube/maths/random.hpp>
+#include <qube/approximations.hpp>
+#include <qube/decompositions.hpp>
 
 using namespace qube::stream;
 
@@ -194,4 +196,116 @@ TEST(chapter4_5, H_T_phase_CNOT_universality_3)
     auto const error = qube::maths::operator_norm_2(Rn_alpha - Rn_approx);
 
     EXPECT_LT(error, epsilon / 3.);
+}
+
+//! @brief Equations 4.78 and 4.79
+TEST(chapter4_5, H_T_phase_CNOT_universality_4)
+{
+    qube::maths::seed();
+
+    auto constexpr pi = std::numbers::pi;
+    auto constexpr c = std::cos(pi / 8.);
+    auto constexpr s = std::sin(pi / 8.);
+
+    auto n = std::array{c, s, c};
+    Eigen::Vector3d::Map(n.data()).normalize();
+    auto m = std::array{c,-s, c};
+    Eigen::Vector3d::Map(m.data()).normalize();
+
+    auto const alpha = qpp::rand(0., 2. * std::numbers::pi);
+    auto const& H = qpp::gt.H;
+
+    auto const expected_Rm = qpp::gt.Rn(alpha, m).eval();
+    auto const Rm = (H * qpp::gt.Rn(alpha, n) * H).eval();
+    EXPECT_MATRIX_CLOSE(Rm, expected_Rm, 1e-12);
+
+    debug() << ">> Rm:\n" << qpp::disp(Rm) << "\n\n";
+    debug() << ">> expected_Rm:\n" << qpp::disp(expected_Rm) << "\n\n";
+}
+
+//! @brief Equations 4.80 and 4.81
+//! @details Equation 4.80 is not verified in general.
+//! @see Errata of N&C: https://michaelnielsen.org/qcqi/errata/errata/errata.html, for the correct formula
+//! @details However, it holds on some necessary and sufficient condition: @see generalized_euler_decomposition_exists
+TEST(chapter4_5, H_T_phase_CNOT_universality_5)
+{
+    using namespace std::complex_literals;
+
+    qube::maths::seed(1754132672u);
+
+    auto constexpr pi = std::numbers::pi;
+    auto constexpr c = std::cos(pi / 8.);
+    auto constexpr s = std::sin(pi / 8.);
+
+    auto n = std::array{c, s, c};
+    auto n_v = Eigen::Vector3d::Map(n.data());
+    n_v.normalize();
+    auto m = std::array{c,-s, c};
+    auto m_v = Eigen::Vector3d::Map(m.data());
+    m_v.normalize();
+
+    auto const U = qpp::randU();
+    auto const [phase_U, theta_U, n_U] = qube::unitary_to_rotation(U);
+    auto alpha = qube::generalized_euler_decomposition(phase_U, theta_U, n_U, n_v, m_v, n_v);
+
+    auto const computed_U = (std::exp(1.i * alpha[0])
+        * qpp::gt.Rn(alpha[1], n)
+        * qpp::gt.Rn(alpha[2], m)
+        * qpp::gt.Rn(alpha[3], n)).eval();
+    EXPECT_MATRIX_CLOSE(computed_U, U, 1e-12);
+
+    auto constexpr theta = 2. * std::acos(c*c);
+    // Don't ask for too high a precision, otherwise the test will reach floating-point precision limits
+    auto constexpr epsilon = 1e-4;
+
+    auto constexpr beta = 4. * std::asin(epsilon / 6.);
+    auto constexpr delta = beta;
+
+    auto alpha_approx = Eigen::Vector3d::Zero().eval();
+    auto mk = Eigen::Vector3<unsigned long int>::Zero().eval();
+    auto theta_k = Eigen::Vector3d::Zero().eval();
+
+    for (auto&& i: std::views::iota(0, 3))
+    {
+        debug() << "\n";
+        alpha[i+1] = std::fmod(alpha[i+1], 2. * pi);
+        if (alpha[i+1] < 0.)
+        {
+            alpha[i+1] += 2. * pi;
+            // Adding 2pi to alpha[i+1] multiplies by the corresponding rotation matrix by -1.
+            // This equivalent to multiplying the global phase by -1. To compensate, we add pi to alpha[0].
+            alpha[0] += pi;
+        }
+        std::tie(alpha_approx[i], mk[i], theta_k[i]) = qube::angle_approximation(alpha[i+1], theta, delta);
+        EXPECT_LT(std::abs(theta_k[i]), delta);
+        EXPECT_NE(theta_k[i], 0.);
+    }
+
+    auto const& H = qpp::gt.H;
+
+    for (auto&& i: std::views::iota(0, 3))
+    {
+        auto R_approx = qpp::gt.Rn(alpha_approx[i], n).eval();
+        if (i == 1)
+            R_approx = H * R_approx * H;
+        auto const R = qpp::gt.Rn(alpha[i+1], (i == 1 ? m : n)).eval();
+        auto const error = qube::maths::operator_norm_2(R - R_approx);
+        EXPECT_LT(error, epsilon / 3.);
+
+        debug() << ">> R_approx:\n" << qpp::disp(R_approx) << "\n\n";
+        debug() << ">> R:\n" << qpp::disp(R) << "\n\n";
+        debug() << ">> error: " << error << "\n";
+    }
+
+    auto const U_approx = (std::exp(1.i * alpha[0])
+        * qpp::gt.Rn(alpha_approx[0], n)
+        * H * qpp::gt.Rn(alpha_approx[1], n) * H
+        * qpp::gt.Rn(alpha_approx[2], n)).eval();
+
+    auto const error = qube::maths::operator_norm_2(U - U_approx);
+    EXPECT_LT(error, epsilon);
+
+    debug() << ">> U:\n" << qpp::disp(U) << "\n\n";
+    debug() << ">> U_approx:\n" << qpp::disp(U_approx) << "\n\n";
+    debug() << ">> error: " << error << "\n";
 }
